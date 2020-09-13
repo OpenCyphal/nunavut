68470092265b81b4834784b69871114a5ead2219
Scott <dixonsco@amazon.com>
2020-09-13 10:56:20 -0700

    Fixing some foibles and writing copybits tests

--- verification/c/suite/test_support.c
+++ verification/c/suite/test_support.c
@@ -7,9 +7,58 @@
 #include <assert.h>
 #include "nunavut/support/serialization.h"
 
-static void test_foo(void)
+static void testNunavutCopyBits(void)
 {
-    TEST_ASSERT_EQUAL(0L, 0L);
+    const uint8_t src[] = { 1, 2, 3, 4, 5 };
+    uint8_t dst[6];
+    memset(dst, 0, sizeof(dst));
+    nunavutCopyBits(sizeof(src) * 8, 0, 0, src, dst);
+    for(size_t i = 0; i < sizeof(src); ++i)
+    {
+        TEST_ASSERT_EQUAL_UINT8(src[i], dst[i]);
+    }
+}
+
+static void testNunavutCopyBitsWithAlignedOffset(void)
+{
+    const uint8_t src[] = { 1, 2, 3, 4, 5 };
+    uint8_t dst[6];
+    memset(dst, 0, sizeof(dst));
+    nunavutCopyBits(sizeof(src) * 8, 8, 0, src, dst);
+    for(size_t i = 0; i < sizeof(src) - 1; ++i)
+    {
+        TEST_ASSERT_EQUAL_UINT8(src[i + 1], dst[i]);
+    }
+    TEST_ASSERT_EQUAL_UINT8(0, dst[sizeof(dst) - 1]);
+
+    memset(dst, 0, sizeof(dst));
+    nunavutCopyBits(sizeof(src) * 8, 0, 8, src, dst);
+    for(size_t i = 0; i < sizeof(src) - 1; ++i)
+    {
+        TEST_ASSERT_EQUAL_UINT8(src[i], dst[i+1]);
+    }
+    TEST_ASSERT_EQUAL_UINT8(0, dst[0]);
+}
+
+static void testNunavutCopyBitsWithUnalignedOffset(void)
+{
+    const uint8_t src[] = { 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA };
+    uint8_t dst[6];
+    memset(dst, 0, sizeof(dst));
+    nunavutCopyBits(sizeof(src) * 8, 1, 0, src, dst);
+    for(size_t i = 0; i < sizeof(src) - 1; ++i)
+    {
+        TEST_ASSERT_EQUAL_HEX8(0x55, dst[i]);
+    }
+    TEST_ASSERT_EQUAL_HEX8(0x55, dst[sizeof(dst) - 1]);
+
+    memset(dst, 0, sizeof(dst));
+    nunavutCopyBits(sizeof(src) * 8, 0, 1, src, dst);
+    for(size_t i = 0; i < sizeof(src) - 1; ++i)
+    {
+        TEST_ASSERT_EQUAL_HEX8((i == 0) ? 0x54 : 0x55, dst[i]);
+    }
+    TEST_ASSERT_EQUAL_HEX8(0x54, dst[0]);
 }
  
 void setUp(void)
@@ -26,7 +75,9 @@ int main(void)
 {
     UNITY_BEGIN();
  
-    RUN_TEST(test_foo);
+    RUN_TEST(testNunavutCopyBits);
+    RUN_TEST(testNunavutCopyBitsWithAlignedOffset);
+    RUN_TEST(testNunavutCopyBitsWithUnalignedOffset);
  
     return UNITY_END();
 }
