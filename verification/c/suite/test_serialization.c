e9817786e02bdd305d5910b8f11e63553a8fba0b
David Lenfesty <lenfesty@ualberta.ca>
2020-09-15 22:03:57 -0600

    lang/c: started on serialization tests

---
+++ verification/c/suite/test_serialization.c
@@ -0,0 +1,56 @@
+/// Copyright 2020 David Lenfesty
+///
+/// Tests different cases in serialization code.
+///
+#include "unity.h"
+#include <assert.h>
+
+#include "uavcan/primitive/scalar/Bit_1_0.h"
+#include "uavcan/_register/Name_1_0.h"
+
+/// Void fields must explicitely be zeroed by serialization.
+/// Ensures that uninitialized memory in buffer does not "leak".
+static void testVoidZeroed(void)
+{
+    uavcan_primitive_scalar_Bit_1_0 subject;
+    uint8_t buffer[1] = {0xFF};
+
+    uavcan_primitive_scalar_Bit_1_0_init(&subject);
+    (void) uavcan_primitive_scalar_Bit_1_0_serialize(&subject, 0, buffer);
+
+    TEST_ASSERT_EQUAL_HEX8(0x00, (buffer[0] & 0x7F));
+}
+
+/// Ensures that variable length primitive arrays copy correctly.
+static void testVariablePrimitiveArrayLength(void)
+{
+    uavcan_register_Name_1_0 subject;
+    uint8_t buffer[uavcan_register_Name_1_0_MAX_SERIALIZED_REPRESENTATION_SIZE_BYTES];
+
+    uavcan_register_Name_1_0_init(&subject);
+    subject.name_length = strlen("foo");
+    strcpy((char*)subject.name, "foo");
+    uavcan_register_Name_1_0_serialize(&subject, 0, buffer);
+
+    TEST_ASSERT_EQUAL_STRING_LEN("foo", (char*)&buffer[1], strlen("foo"));
+}
+
+void setUp(void)
+{
+
+}
+
+void tearDown(void)
+{
+
+}
+
+int main(void)
+{
+    UNITY_BEGIN();
+
+    RUN_TEST(testVoidZeroed);
+    RUN_TEST(testVariablePrimitiveArrayLength);
+
+    return UNITY_END();
+}
