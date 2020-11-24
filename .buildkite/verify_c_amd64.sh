7e12a1c68e9509baeffb3c4715c459783754b76b
Pavel Kirienko <pavel.kirienko@gmail.com>
2020-11-24 02:01:23 +0200

    Simplify the verification shell scripts

--- .buildkite/verify_c_amd64.sh
+++ .buildkite/verify_c_amd64.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
 set -o errexit
-set -o nounset
-set -o xtrace
-
 cd "${0%/*}/.."
 
 function run()
@@ -11,11 +8,6 @@ function run()
     .buildkite/verify.py --verbose --force --language c $*
 }
 
-# AMD64
 run  --endianness little  --platform native64  --build-type Debug
 run  --endianness little  --platform native64  --build-type Release
 run  --endianness little  --platform native64  --build-type MinSizeRel  --disable-asserts
-
-# TODO: ARM
-# TODO: a big-endian architecture
-# TODO: an 8-bit architecture
--- .buildkite/verify_c_x86.sh
+++ .buildkite/verify_c_x86.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
 set -o errexit
-set -o nounset
-set -o xtrace
-
 cd "${0%/*}/.."
 
 function run()
@@ -11,11 +8,6 @@ function run()
     .buildkite/verify.py --verbose --force --language c $*
 }
 
-# x86
-run  --endianness any     --platform native32  --build-type Debug
-run  --endianness any     --platform native32  --build-type Release
-run  --endianness any     --platform native32  --build-type MinSizeRel  --disable-asserts
-
-# TODO: ARM
-# TODO: a big-endian architecture
-# TODO: an 8-bit architecture
+run  --endianness any  --platform native32  --build-type Debug
+run  --endianness any  --platform native32  --build-type Release
+run  --endianness any  --platform native32  --build-type MinSizeRel  --disable-asserts
