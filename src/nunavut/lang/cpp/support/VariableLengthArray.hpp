34f20d62064805d0630ba87898df41b1ccf1f83d
Scott <dixonsco@amazon.com>
2021-02-28 15:20:43 -0800

    good progress on the variable length array type

--- src/nunavut/lang/cpp/support/VariableLengthArray.hpp
+++ src/nunavut/lang/cpp/support/VariableLengthArray.hpp
@@ -5,21 +5,20 @@
  * This software is distributed under the terms of the MIT License.
  */
 
-
 #ifndef NUNAVUT_SUPPORT_VARIABLE_LENGTH_ARRAY_HPP_INCLUDED
 #define NUNAVUT_SUPPORT_VARIABLE_LENGTH_ARRAY_HPP_INCLUDED
 
 #include <cstddef>
 #include <cstring>
+#include <type_traits>
+#include <memory>
+#include <utility>
 
 namespace nunavut
 {
 namespace support
 {
-
-template <typename T,
-          typename Allocator,
-          std::size_t MaxSize>
+template <typename T, typename Allocator, std::size_t MaxSize>
 class VariableLengthArray
 {
 public:
@@ -30,12 +29,12 @@ public:
         , alloc_()
     {}
 
-    using allocator_type = Allocator;
-
-    constexpr allocator_type get_allocator() const noexcept
+    ~VariableLengthArray()
     {
-        return alloc_;
+        fast_deallocate(data_, size_, capacity_, alloc_);
     }
+
+    using allocator_type = Allocator;
 
     // +----------------------------------------------------------------------+
     // | ELEMENT ACCESS
@@ -45,20 +44,44 @@ public:
         return data_;
     }
 
-    constexpr const T& operator[](std::size_t i) const
+    constexpr const T& operator[](std::size_t i) const noexcept
     {
         return data_[i];
     }
 
-    constexpr T& operator[](std::size_t i)
+    constexpr T& operator[](std::size_t i) noexcept
     {
         return data_[i];
+    }
+
+    constexpr T* at_or_null(std::size_t pos) noexcept
+    {
+        if (pos < size_)
+        {
+            return &data_[pos];
+        }
+        else
+        {
+            return nullptr;
+        }
+    }
+
+    constexpr const T* at_or_null(std::size_t pos) const noexcept
+    {
+        if (pos < size_)
+        {
+            return &data_[pos];
+        }
+        else
+        {
+            return nullptr;
+        }
     }
 
     // +----------------------------------------------------------------------+
     // | CAPACITY
     // +----------------------------------------------------------------------+
-    constexpr std::size_t max_size() const
+    constexpr std::size_t max_size() const noexcept
     {
         return MaxSize;
     }
@@ -73,50 +96,74 @@ public:
         return size_;
     }
 
-    constexpr std::size_t reserve(const std::size_t desired_capacity) noexcept
+    std::size_t reserve(const std::size_t desired_capacity) noexcept
     {
-        const std::size_t old_data_size = size_;
-        T* const old_data = data_;
+        const std::size_t clamped_capacity   = (desired_capacity > MaxSize) ? MaxSize : desired_capacity;
+        const std::size_t no_shrink_capacity = (clamped_capacity > size_) ? clamped_capacity : size_;
 
-        const std::size_t clamped_capacity = (desired_capacity > MaxSize) ? MaxSize : desired_capacity;
-        const std::size_t no_shrink_capacity = (clamped_capacity > old_data_size) ? clamped_capacity : old_data_size;
-        T* new_data = alloc_.allocate(no_shrink_capacity);
+        T* new_data = nullptr;
+        try
+        {
+            new_data = alloc_.allocate(no_shrink_capacity);
+        } catch (const std::bad_array_new_length& e)
+        {
+            // we ignore the exception since all allocation failures are modeled using
+            // null by this class.
+            (void) e;
+        }
 
         if (new_data != nullptr)
         {
-            if (old_data_size > 0 && new_data != old_data)
+            if (new_data != data_)
             {
-                // The allocator returned new memory. Copy any initialized objects in the old region to the new one.
-                // Check for memory overlap.
-                const std::size_t old_data_size_bytes = (old_data_size * sizeof(T));
-                if (
-                    (old_data + old_data_size_bytes < new_data)
-                        ||
-                    (new_data + old_data_size_bytes < old_data)
-                )
-                {
-                    // Initialized regions do not overlap. Use memcpy.
-                    std::memcpy(new_data, old_data, old_data_size_bytes);
-                }
-                else
-                {
-                    // Initialized regions overlap. Use memmove.
-                    std::memmove(new_data, old_data, old_data_size_bytes);
-                }
-            }
-
-            data_ = new_data;
+                move_and_free(new_data, data_, size_, capacity_, alloc_);
+            }  // else the allocator was able to simply extend the reserved area for the same memory pointer.
+            data_     = new_data;
             capacity_ = no_shrink_capacity;
         }
 
         return capacity_;
     }
 
+    bool shrink_to_fit()
+    {
+        if (size_ == capacity_)
+        {
+            // already shrunk
+            return true;
+        }
+        // Allocate only enough to store what we have.
+        T* minimized_data = nullptr;
+
+        try
+        {
+            minimized_data = alloc_.allocate(size_ * sizeof(T));
+        } catch (const std::bad_array_new_length& e)
+        {
+            // we ignore the exception since all allocation failures are modeled using
+            // null by this class.
+            (void) e;
+        }
+        if (minimized_data == nullptr)
+        {
+            return false;
+        }
+        else
+        {
+            if (minimized_data != data_)
+            {
+                move_and_free(minimized_data, data_, size_, capacity_, alloc_);
+            }  // else the allocator was able to simply shrink the reserved area for the same memory pointer.
+            data_     = minimized_data;
+            capacity_ = size_;
+            return true;
+        }
+    }
 
     // +----------------------------------------------------------------------+
     // | MODIFIERS
     // +----------------------------------------------------------------------+
-    constexpr T* push_back_no_alloc( T&& value ) noexcept
+    constexpr T* push_back_no_alloc(T&& value)
     {
         if (size_ < capacity_)
         {
@@ -128,7 +175,7 @@ public:
         }
     }
 
-    constexpr T* push_back_no_alloc( const T& value ) noexcept
+    constexpr T* push_back_no_alloc(const T& value)
     {
         if (size_ < capacity_)
         {
@@ -137,17 +184,133 @@ public:
         else
         {
             return nullptr;
+        }
+    }
+
+    constexpr void pop_back() noexcept
+    {
+        static_assert(std::is_nothrow_destructible<T>::value,
+                      "This container cannot contain objects with destructors that may throw exceptions.");
+        if (size_ > 0)
+        {
+            data_[size_--].~T();
         }
     }
 
 private:
-    T* data_;
+    /**
+     * If trivially destructible then we don't have to call the destructors.
+     */
+    template <typename U>
+    static constexpr void fast_deallocate(
+        U* const    src,
+        std::size_t src_size_count,
+        std::size_t src_capacity_count,
+        Allocator&  alloc,
+        typename std::enable_if<std::is_trivially_destructible<U>::value>::type* = 0) noexcept
+    {
+        (void) src_size_count;
+        alloc.deallocate(src, src_capacity_count);
+    }
+
+    /**
+     * If not trivially destructible then we invoke each destructor.
+     */
+    template <typename U>
+    static constexpr void fast_deallocate(
+        U* const    src,
+        std::size_t src_size_count,
+        std::size_t src_capacity_count,
+        Allocator&  alloc,
+        typename std::enable_if<!std::is_trivially_destructible<U>::value>::type* = 0) noexcept
+    {
+        static_assert(std::is_nothrow_destructible<U>::value,
+                      "This container cannot contain objects with destructors that may throw exceptions.");
+        for (std::size_t i = 0; i < src_size_count; ++i)
+        {
+            src[i].~U();
+        }
+        alloc.deallocate(src, src_capacity_count);
+    }
+
+    /**
+     * Move stuff in src to dst and then free all the memory allocated for src.
+     */
+    template <typename U>
+    static constexpr void move_and_free(U* const    dst,
+                                        U* const    src,
+                                        std::size_t src_len_count,
+                                        std::size_t src_capacity_count,
+                                        Allocator&  alloc,
+                                        typename std::enable_if<std::is_fundamental<U>::value>::type*     = 0,
+                                        typename std::enable_if<std::is_move_assignable<U>::value>::type* = 0) noexcept
+    {
+        if (src_len_count > 0)
+        {
+            // The allocator returned new memory. Copy any initialized objects in the old region to the new one.
+            std::memcpy(dst, src, src_len_count * sizeof(U));
+        }
+        fast_deallocate(src, src_len_count, src_capacity_count, alloc);
+    }
+
+    /**
+     * Same as above but for non-fundamental types. We can't just memcpy for these.
+     */
+    template <typename U>
+    static constexpr void move_and_free(
+        U* const    dst,
+        U* const    src,
+        std::size_t src_len_count,
+        std::size_t src_capacity_count,
+        Allocator&  alloc,
+        typename std::enable_if<!std::is_fundamental<U>::value>::type*               = 0,
+        typename std::enable_if<std::is_nothrow_move_constructible<U>::value>::type* = 0) noexcept
+    {
+        if (src_len_count > 0)
+        {
+            // The allocator returned new memory. Copy any initialized objects in the old region to the new one.
+            for (size_t i = 0; i < src_len_count; ++i)
+            {
+                new (&dst[i]) U(std::move(src[i]));
+            }
+        }
+        fast_deallocate(src, src_len_count, src_capacity_count, alloc);
+    }
+
+    /**
+     * Same as non-fundamental move but for objects that don't support move constructors.
+     */
+    template <typename U>
+    static constexpr void move_and_free(
+        U* const    dst,
+        U* const    src,
+        std::size_t src_len_count,
+        std::size_t src_capacity_count,
+        Allocator&  alloc,
+        typename std::enable_if<!std::is_fundamental<U>::value>::type*                = 0,
+        typename std::enable_if<!std::is_nothrow_move_constructible<U>::value>::type* = 0) noexcept
+    {
+        static_assert(std::is_nothrow_copy_constructible<U>::value,
+                      "This container cannot be used with non-fundamental objects that do not have a copy or move "
+                      "constructor that does not throw exceptions.");
+        if (src_len_count > 0)
+        {
+            // The allocator returned new memory. Copy any initialized objects in the old region to the new one.
+            for (size_t i = 0; i < src_len_count; ++i)
+            {
+                new (&dst[i]) U(src[i]);
+            }
+        }
+        fast_deallocate(src, src_len_count, src_capacity_count, alloc);
+    }
+
+    T*          data_;
     std::size_t capacity_;
     std::size_t size_;
-    Allocator alloc_;
+    Allocator   alloc_;
 };
 
-} // namespace nunavut
-} // namespace support
+}  // namespace support
+}  // namespace nunavut
 
-#endif // NUNAVUT_SUPPORT_VARIABLE_LENGTH_ARRAY_HPP_INCLUDED
+#endif  // NUNAVUT_SUPPORT_VARIABLE_LENGTH_ARRAY_HPP_INCLUDED
--- verification/cpp/suite/test_var_len_arr.cpp
+++ verification/cpp/suite/test_var_len_arr.cpp
@@ -8,12 +8,14 @@
 #include "nunavut/support/VariableLengthArray.hpp"
 #include <memory>
 #include <type_traits>
+#include <limits>
 #include "o1heap/o1heap.h"
+#include <string>
 
 /**
  * Pavel's O(1) Heap Allocator wrapped in an std::allocator concept.
  */
-template<typename T, std::size_t SizeCount>
+template <typename T, std::size_t SizeCount>
 class O1HeapAllocator
 {
 public:
@@ -22,23 +24,28 @@ public:
     O1HeapAllocator()
         : heap_()
         , heap_alloc_(o1heapInit(&heap_[0], SizeCount * sizeof(T), nullptr, nullptr))
-    {
-    }
+    {}
 
-    T* allocate( std::size_t n )
+    T* allocate(std::size_t n)
     {
         return reinterpret_cast<T*>(o1heapAllocate(heap_alloc_, n * sizeof(T)));
     }
 
+    constexpr void deallocate(T* p, std::size_t n)
+    {
+        (void) n;
+        o1heapFree(heap_alloc_, p);
+    }
+
 private:
     typename std::aligned_storage<sizeof(T), O1HEAP_ALIGNMENT>::type heap_[SizeCount];
-    O1HeapInstance* heap_alloc_;
+    O1HeapInstance*                                                  heap_alloc_;
 };
 
 /**
  * A Junky static allocator.
  */
-template<typename T, std::size_t SizeCount>
+template <typename T, std::size_t SizeCount>
 class JunkyStaticAllocator
 {
 public:
@@ -46,10 +53,9 @@ public:
 
     JunkyStaticAllocator()
         : data_()
-    {
-    }
+    {}
 
-    T* allocate( std::size_t n )
+    T* allocate(std::size_t n)
     {
         if (n < SizeCount)
         {
@@ -59,6 +65,13 @@ public:
         {
             return nullptr;
         }
+    }
+
+    constexpr void deallocate(T* p, std::size_t n)
+    {
+        // This allocator is junk.
+        (void) p;
+        (void) n;
     }
 
 private:
@@ -71,36 +84,70 @@ private:
  */
 template <typename T>
 class VLATestsGeneric : public ::testing::Test
-{
-};
+{};
 
 using VLATestsGenericAllocators = ::testing::Types<std::allocator<int>,
-                                                           std::allocator<long long>,
-                                                           O1HeapAllocator<int, O1HEAP_ALIGNMENT * 8>,
-                                                           JunkyStaticAllocator<int, 10>>;
-TYPED_TEST_SUITE(VLATestsGeneric, VLATestsGenericAllocators,);
-
-
+                                                   std::allocator<long long>,
+                                                   O1HeapAllocator<int, O1HEAP_ALIGNMENT * 8>,
+                                                   JunkyStaticAllocator<int, 30>>;
+TYPED_TEST_SUITE(VLATestsGeneric, VLATestsGenericAllocators, );
 
 TYPED_TEST(VLATestsGeneric, TestReserve)
 {
     nunavut::support::VariableLengthArray<typename TypeParam::value_type, TypeParam, 10> subject;
-    ASSERT_EQ(nullptr, subject.data());
     ASSERT_EQ(0U, subject.capacity());
     ASSERT_EQ(0U, subject.size());
     ASSERT_EQ(10U, subject.max_size());
-    ASSERT_EQ(nullptr, subject.push_back_no_alloc(1));
 
     ASSERT_EQ(1U, subject.reserve(1));
 
     ASSERT_EQ(1U, subject.capacity());
     ASSERT_EQ(0U, subject.size());
     ASSERT_EQ(10U, subject.max_size());
+}
+
+TYPED_TEST(VLATestsGeneric, TestPush)
+{
+    nunavut::support::VariableLengthArray<typename TypeParam::value_type, TypeParam, 20> subject;
+    ASSERT_EQ(nullptr, subject.data());
+    ASSERT_EQ(nullptr, subject.push_back_no_alloc(1));
+
+    ASSERT_EQ(10U, subject.reserve(10));
+
+    ASSERT_EQ(10U, subject.capacity());
+    ASSERT_EQ(0U, subject.size());
+    ASSERT_EQ(20U, subject.max_size());
     const typename TypeParam::value_type* const pushed = subject.push_back_no_alloc(1);
+    ASSERT_NE(nullptr, pushed);
     ASSERT_EQ(*pushed, 1);
     ASSERT_EQ(1U, subject.size());
 }
 
+TYPED_TEST(VLATestsGeneric, TestPop)
+{
+    nunavut::support::VariableLengthArray<typename TypeParam::value_type, TypeParam, 20> subject;
+    ASSERT_EQ(10U, subject.reserve(10));
+    const typename TypeParam::value_type* const pushed = subject.push_back_no_alloc(1);
+    ASSERT_NE(nullptr, pushed);
+    ASSERT_EQ(*pushed, 1);
+    ASSERT_EQ(1U, subject.size());
+    subject.pop_back();
+    ASSERT_EQ(0U, subject.size());
+    ASSERT_EQ(10U, subject.capacity());
+}
+
+TYPED_TEST(VLATestsGeneric, TestShrink)
+{
+    nunavut::support::VariableLengthArray<typename TypeParam::value_type, TypeParam, 20> subject;
+    ASSERT_EQ(10U, subject.reserve(10));
+    const typename TypeParam::value_type* const pushed = subject.push_back_no_alloc(1);
+    ASSERT_NE(nullptr, pushed);
+    ASSERT_EQ(*pushed, 1);
+    ASSERT_EQ(1U, subject.size());
+    ASSERT_EQ(10U, subject.capacity());
+    subject.shrink_to_fit();
+    ASSERT_EQ(1U, subject.capacity());
+}
 
 // +----------------------------------------------------------------------+
 /**
@@ -108,38 +155,102 @@ TYPED_TEST(VLATestsGeneric, TestReserve)
  */
 template <typename T>
 class VLATestsStatic : public ::testing::Test
-{
-};
+{};
 
-using VLATestsStaticAllocators = ::testing::Types<O1HeapAllocator<int, O1HEAP_ALIGNMENT * 8>,
-                                                           JunkyStaticAllocator<int, 10>>;
-TYPED_TEST_SUITE(VLATestsStatic, VLATestsStaticAllocators,);
+using VLATestsStaticAllocators =
+    ::testing::Types<O1HeapAllocator<int, O1HEAP_ALIGNMENT * 8>, JunkyStaticAllocator<int, 10>>;
+TYPED_TEST_SUITE(VLATestsStatic, VLATestsStaticAllocators, );
 
 TYPED_TEST(VLATestsStatic, TestOutOfMemory)
 {
-    nunavut::support::VariableLengthArray<typename TypeParam::value_type, TypeParam, 10> subject;
+    nunavut::support::
+        VariableLengthArray<typename TypeParam::value_type, TypeParam, std::numeric_limits<std::size_t>::max()>
+            subject;
     ASSERT_EQ(0U, subject.capacity());
 
-    std::size_t last_capacity = subject.capacity();
-    bool did_run_out_of_memory = false;
-    std::size_t ran_out_of_memory_at = 0;
-    for(std::size_t i = 1; i <= 1024; ++i)
+    bool        did_run_out_of_memory = false;
+    std::size_t ran_out_of_memory_at  = 0;
+    for (std::size_t i = 1; i <= 1024; ++i)
     {
-        if (last_capacity == subject.reserve(i))
+        ASSERT_EQ(i - 1, subject.size());
+        if (subject.reserve(i) < i)
         {
             did_run_out_of_memory = true;
-            ran_out_of_memory_at = i - 1;
+            ran_out_of_memory_at  = i;
             break;
         }
-        last_capacity = subject.capacity();
-        typename TypeParam::value_type* pushed = subject.push_back_no_alloc(static_cast<typename TypeParam::value_type>(i));
+        ASSERT_EQ(i, subject.capacity());
+        typename TypeParam::value_type* pushed =
+            subject.push_back_no_alloc(static_cast<typename TypeParam::value_type>(i));
         ASSERT_NE(nullptr, pushed);
         ASSERT_EQ(static_cast<typename TypeParam::value_type>(i), *pushed);
     }
     ASSERT_TRUE(did_run_out_of_memory);
-    ASSERT_EQ(nullptr,subject.push_back_no_alloc(0));
-    for(std::size_t i = 0; i < ran_out_of_memory_at; ++i)
+    ASSERT_EQ(nullptr, subject.push_back_no_alloc(0));
+    for (std::size_t i = 1; i < ran_out_of_memory_at; ++i)
     {
-        ASSERT_EQ(static_cast<typename TypeParam::value_type>(i+1), subject[i]);
+        ASSERT_EQ(static_cast<typename TypeParam::value_type>(i), subject[i - 1]);
     }
+}
+
+TYPED_TEST(VLATestsStatic, TestOverMaxSize)
+{
+    static constexpr std::size_t MaxSize = 5;
+    static_assert(MaxSize > 0, "Test assumes MaxSize > 0");
+    nunavut::support::VariableLengthArray<typename TypeParam::value_type, TypeParam, MaxSize> subject;
+    ASSERT_EQ(0U, subject.capacity());
+
+    for (std::size_t i = 1; i <= MaxSize; ++i)
+    {
+        ASSERT_EQ(i, subject.reserve(i));
+        typename TypeParam::value_type* pushed =
+            subject.push_back_no_alloc(static_cast<typename TypeParam::value_type>(i));
+        ASSERT_NE(nullptr, pushed);
+        ASSERT_EQ(static_cast<typename TypeParam::value_type>(i), *pushed);
+    }
+    ASSERT_EQ(MaxSize, subject.reserve(MaxSize + 1));
+    ASSERT_EQ(nullptr, subject.push_back_no_alloc(0));
+    for (std::size_t i = 0; i < MaxSize; ++i)
+    {
+        ASSERT_EQ(static_cast<typename TypeParam::value_type>(i + 1), subject[i]);
+    }
+}
+
+// +----------------------------------------------------------------------+
+/**
+ * Test suite to ensure non-trivial objects are properly handled.
+ */
+
+TEST(VLATestsNonTrivial, TestDeallocSize)
+{
+    // TODO: be sure that the dealloc method is called with the same size
+    // used for alloc
+}
+
+TEST(VLATestsNonTrivial, TestDestroy)
+{
+    // TODO: ensure data item destructors are called if container is deleted.
+}
+
+TEST(VLATestsNonTrival, TestNonFunamental)
+{
+    nunavut::support::VariableLengthArray<std::string, std::allocator<std::string>, 10> subject;
+    ASSERT_EQ(10U, subject.reserve(10));
+}
+
+TEST(VLATestsNonTrival, TestNotMovable)
+{
+    class NotMovable
+    {
+    public:
+        NotMovable() {}
+        NotMovable(NotMovable&&) = delete;
+        NotMovable(const NotMovable& rhs) noexcept
+        {
+            (void)rhs;
+        }
+    };
+    nunavut::support::VariableLengthArray<NotMovable, std::allocator<NotMovable>, 10> subject;
+    ASSERT_EQ(10U, subject.reserve(10));
+    ASSERT_NE(nullptr, subject.push_back_no_alloc(NotMovable()));
 }
