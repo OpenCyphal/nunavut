73340f8b737c9cb7959cd8c893a57bb305fcf4fd
bbworld <vwangsf@gmail.com>
2021-05-02 02:49:17 -0700

    Implement collapse nested on collapse

---
+++ src/nunavut/lang/docgen/templates/namespace_base.js
@@ -0,0 +1,37 @@
+function collapseOn(collapse) {
+  collapse.classList.add("show");
+}
+
+function collapseOff(collapse) {
+  collapse.classList.remove("show");
+}
+
+function showCollapse(collapse) {
+  collapseOn(collapse);
+  linkFromCollapse(collapse).innerText = "-";
+  if (collapse.parentNode != null && collapse.classList.contains("collapse")) {
+    showCollapse(collapse.parentNode);
+  }
+}
+
+function toggleCollapse(e) {
+  let collapse = document.querySelector(e.target.dataset.target);
+  console.log(collapse, e)
+  if (collapse.classList.contains("show")) {
+    // collapse
+    for (let el of collapse.querySelectorAll(".collapse.show")) {
+      collapseOff(el);
+      linkFromCollapse(el).innerText = "+";
+    }
+    collapseOff(collapse);
+    e.target.innerText = "+";
+  } else {
+    // show
+    showCollapse(collapse);
+    e.target.innerText = "-";
+  }
+}
+
+function linkFromCollapse(collapse) {
+  return collapse.previousSibling.previousSibling.querySelector("a");
+}
