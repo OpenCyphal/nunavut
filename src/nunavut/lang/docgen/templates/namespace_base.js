f22a3df4e78ca7e7ca657051de6cd69ab31b5ed9
Vincent Wang <vwangsf@gmail.com>
2021-06-15 00:51:13 -0700

    Make sidebar follow the main view (scroll to opened datatype)

--- src/nunavut/lang/docgen/templates/namespace_base.js
+++ src/nunavut/lang/docgen/templates/namespace_base.js
@@ -34,16 +34,27 @@ function toggleCollapse(e, type_tag) {
     e.target.innerText = "-";
   }
 
+  console.log(type_tag)
   scrollSidebar(type_tag);
 }
 
 function linkFromCollapse(collapse) {
-  return collapse.previousSibling.previousSibling.querySelector("a");
+  try {
+    return collapse.previousSibling.previousSibling.querySelector("a");
+  } catch (e) {
+    return linkFromCollapse(collapse.parentNode.parentNode);
+  }
 }
 
 function scrollSidebar(type) {
   let el = document.getElementById(`${type}_sidebar`);
   console.log("scroll sidebar", el);
-  showCollapse(document.getElementById(`${type}_sidebar`));
-  el.scrollIntoView();
+
+  if (el.nodeName === "div") {
+    showCollapse(el);
+    linkFromCollapse(el).scrollIntoView();
+  } else {
+    showCollapse(el.parentNode.parentNode);
+    el.scrollIntoView();
+  }
 }
