6114b733b646eeafca7252557a58cc4ad5e1acfd
Scott Dixon <dixonsco@amazon.com>
2021-12-14 11:02:55 -0800

    huh, not sure why this breaks

--- verification/c/suite/test_override_variable_array_capacity.c
+++ verification/c/suite/test_override_variable_array_capacity.c
@@ -3,9 +3,8 @@
 
 
 #if NUNAVUT_SUPPORT_LANGUAGE_OPTION_ENABLE_OVERRIDE_VARIABLE_ARRAY_CAPACITY == 1
-#  define OVERRIDE_SIZE 2
-#  define regulated_basics_PrimitiveArrayVariable_0_1_a_u64_ARRAY_CAPACITY_ OVERRIDE_SIZE
-#  define regulated_basics_PrimitiveArrayVariable_0_1_n_f32_ARRAY_CAPACITY_ OVERRIDE_SIZE
+#  define regulated_basics_PrimitiveArrayVariable_0_1_a_u64_ARRAY_CAPACITY_ 2
+#  define regulated_basics_PrimitiveArrayVariable_0_1_n_f32_ARRAY_CAPACITY_ 2
 #endif
 
 #include <regulated/basics/PrimitiveArrayVariable_0_1.h>
@@ -20,8 +19,8 @@ static void testPrimitiveArrayVariableOverride(void)
     uint8_t buf[regulated_basics_PrimitiveArrayVariable_0_1_SERIALIZATION_BUFFER_SIZE_BYTES_ - 1];
 
 #if NUNAVUT_SUPPORT_LANGUAGE_OPTION_ENABLE_OVERRIDE_VARIABLE_ARRAY_CAPACITY == 1
-    TEST_ASSERT_EQUAL(sizeof(ref.a_u64.elements[0]) * OVERRIDE_SIZE, sizeof(ref.a_u64.elements));
-    TEST_ASSERT_EQUAL(sizeof(ref.n_f64.elements[0]) * OVERRIDE_SIZE, sizeof(ref.n_f64.elements));
+    TEST_ASSERT_EQUAL(sizeof(ref.a_u64.elements[0]) * 2, sizeof(ref.a_u64.elements));
+    TEST_ASSERT_EQUAL(sizeof(ref.n_f64.elements[0]) * 2, sizeof(ref.n_f64.elements));
 #else
     TEST_ASSERT_EQUAL(sizeof(ref.a_u64.elements[0]) *
                       regulated_basics_PrimitiveArrayVariable_0_1_a_u64_ARRAY_CAPACITY_,
