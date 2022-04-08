OpenCyphal/pydsdl

commit ced95efbbc78e3943a88b15dffce580e3dde1b6f

Use pathlib.Path instead of plain string paths (#77)
* Drop Python 3.6 and update dependencies

* Use pathlib.Path instead of raw str throughout; keep API compatible

* Add missing future imports

* Apply non-POSIX normalization

* Fix #71

* Enhance diagnostics to help with macos debugging

* CI: run macos build sooner

* macos-specific fixes

setup.cfg
@@ -116,7 +116,10 @@ disable=
     too-many-statements,
     useless-super-delegation,
     too-many-instance-attributes,
-    too-many-public-methods
+    too-many-public-methods,
+    consider-using-f-string,
+    unspecified-encoding,
+    use-implicit-booleaness-not-comparison
 
 [pylint.REPORTS]
 output-format=colorized

pydsdl/__init__.py
@@ -7,7 +7,7 @@
 import os as _os
 import sys as _sys
 
-__version__ = "1.13.0"
+__version__ = "1.14.0"
 __version_info__ = tuple(map(int, __version__.split(".")[:3]))
 __license__ = "MIT"
 __author__ = "UAVCAN Consortium"

.idea/dictionaries/pavel.xml
@@ -15,6 +15,7 @@
       <w>autodoc</w>
       <w>autosummary</w>
       <w>baudrate</w>
+      <w>booleaness</w>
       <w>bools</w>
       <w>bufferless</w>
       <w>bysource</w>

appveyor.yml
@@ -8,6 +8,10 @@ environment:
       PYTHON: "C:\\Python310-x64"
 
     - job_group: tests
+      APPVEYOR_BUILD_WORKER_IMAGE: macos
+      PYTHON: "3.9"
+
+    - job_group: tests
       APPVEYOR_BUILD_WORKER_IMAGE: Ubuntu2004
       PYTHON: "3.10"
 
@@ -22,14 +26,6 @@ environment:
     - job_group: tests
       APPVEYOR_BUILD_WORKER_IMAGE: Ubuntu2004
       PYTHON: "3.7"
-
-    - job_group: tests
-      APPVEYOR_BUILD_WORKER_IMAGE: Ubuntu2004
-      PYTHON: "3.6"
-
-    - job_group: tests
-      APPVEYOR_BUILD_WORKER_IMAGE: macos
-      PYTHON: "3.9"
 
     - job_name: deploy
       job_depends_on: tests

noxfile.py
@@ -9,7 +9,7 @@ from pathlib import Path
 import nox
 
 
-PYTHONS = ["3.6", "3.7", "3.8", "3.9", "3.10"]
+PYTHONS = ["3.7", "3.8", "3.9", "3.10"]
 """The newest supported Python shall be listed LAST."""
 
 nox.options.error_on_external_run = True
@@ -48,9 +48,9 @@ def test(session):
     session.log("Using the newest supported Python: %s", is_latest_python(session))
     session.install("-e", ".")
     session.install(
-        "pytest          ~= 6.2",
-        "pytest-randomly ~= 3.5",
-        "coverage        ~= 5.5",
+        "pytest          ~= 7.0",
+        "pytest-randomly ~= 3.11",
+        "coverage        ~= 6.3",
     )
     session.run("coverage", "run", "-m", "pytest")
     session.run("coverage", "report", "--fail-under=95")
@@ -65,7 +65,7 @@ def lint(session):
     session.log("Using the newest supported Python: %s", is_latest_python(session))
     session.install(
         "mypy   == 0.931",
-        "pylint == 2.7.2",
+        "pylint == 2.12.*",
     )
     session.run(
         "mypy",
@@ -84,7 +84,7 @@ def lint(session):
         },
     )
     if is_latest_python(session):
-        session.install("black == 21.12b0")
+        session.install("black == 22.*")
         session.run("black", "--check", ".")
 
 

pydsdl/_data_schema_builder.py
@@ -2,7 +2,7 @@
 # This software is distributed under the terms of the MIT License.
 # Author: Pavel Kirienko <pavel@uavcan.org>
 
-import typing
+from __future__ import annotations
 from . import _error
 from . import _serializable
 from . import _bit_length_set
@@ -34,26 +34,26 @@ class SealedSerializationMode(SerializationMode):
 
 class DataSchemaBuilder:
     def __init__(self) -> None:
-        self._fields = []  # type: typing.List[_serializable.Field]
-        self._constants = []  # type: typing.List[_serializable.Constant]
-        self._serialization_mode = None  # type: typing.Optional[SerializationMode]
+        self._fields: list[_serializable.Field] = []
+        self._constants: list[_serializable.Constant] = []
+        self._serialization_mode: SerializationMode | None = None
         self._is_union = False
         self._bit_length_computed_at_least_once = False
         self._doc = ""
 
     @property
-    def fields(self) -> typing.List[_serializable.Field]:
+    def fields(self) -> list[_serializable.Field]:
         assert all(map(lambda x: isinstance(x, _serializable.Field), self._fields))
         return self._fields
 
     @property
-    def constants(self) -> typing.List[_serializable.Constant]:
+    def constants(self) -> list[_serializable.Constant]:
         assert all(map(lambda x: isinstance(x, _serializable.Constant), self._constants))
         return self._constants
 
     @property
-    def attributes(self) -> typing.List[_serializable.Attribute]:  # noinspection PyTypeChecker
-        out = []  # type: typing.List[_serializable.Attribute]
+    def attributes(self) -> list[_serializable.Attribute]:  # noinspection PyTypeChecker
+        out = []  # type: list[_serializable.Attribute]
         out += self.fields
         out += self.constants
         return out
@@ -63,7 +63,7 @@ class DataSchemaBuilder:
         return self._doc
 
     @property
-    def serialization_mode(self) -> typing.Optional[SerializationMode]:
+    def serialization_mode(self) -> SerializationMode | None:
         return self._serialization_mode
 
     @property

pydsdl/_data_type_builder.py
@@ -2,9 +2,9 @@
 # This software is distributed under the terms of the MIT License.
 # Author: Pavel Kirienko <pavel@uavcan.org>
 
-import os
-import typing
+from typing import Optional, Callable, Iterable
 import logging
+from pathlib import Path
 from . import _serializable
 from . import _expression
 from . import _error
@@ -45,15 +45,15 @@ class DataTypeBuilder(_parser.StatementStreamProcessor):
     def __init__(
         self,
         definition: _dsdl_definition.DSDLDefinition,
-        lookup_definitions: typing.Iterable[_dsdl_definition.DSDLDefinition],
-        print_output_handler: typing.Callable[[int, str], None],
+        lookup_definitions: Iterable[_dsdl_definition.DSDLDefinition],
+        print_output_handler: Callable[[int, str], None],
         allow_unregulated_fixed_port_id: bool,
     ):
         self._definition = definition
         self._lookup_definitions = list(lookup_definitions)
         self._print_output_handler = print_output_handler
         self._allow_unregulated_fixed_port_id = allow_unregulated_fixed_port_id
-        self._element_callback = None  # type: typing.Optional[typing.Callable[[str], None]]
+        self._element_callback = None  # type: Optional[Callable[[str], None]]
 
         assert isinstance(self._definition, _dsdl_definition.DSDLDefinition)
         assert all(map(lambda x: isinstance(x, _dsdl_definition.DSDLDefinition), lookup_definitions))
@@ -148,7 +148,7 @@ class DataTypeBuilder(_parser.StatementStreamProcessor):
         )
 
     def on_directive(
-        self, line_number: int, directive_name: str, associated_expression_value: typing.Optional[_expression.Any]
+        self, line_number: int, directive_name: str, associated_expression_value: Optional[_expression.Any]
     ) -> None:
         try:
             handler = {
@@ -209,8 +209,8 @@ class DataTypeBuilder(_parser.StatementStreamProcessor):
                 lookup_nss or "(empty set)",
             )
             if requested_ns not in lookup_nss and requested_ns == subroot_ns:
-                error_description += " Did you mean to use the directory %r instead of %r?" % (
-                    os.path.join(self._definition.root_namespace_path, subroot_ns),
+                error_description += " Did you mean to use the directory %s instead of %s?" % (
+                    self._definition.root_namespace_path / subroot_ns,
                     self._definition.root_namespace_path,
                 )
             else:
@@ -231,7 +231,7 @@ class DataTypeBuilder(_parser.StatementStreamProcessor):
             allow_unregulated_fixed_port_id=self._allow_unregulated_fixed_port_id,
         )
 
-    def _queue_attribute(self, element_callback: typing.Callable[[str], None]) -> None:
+    def _queue_attribute(self, element_callback: Callable[[str], None]) -> None:
         self._flush_attribute("")
         self._element_callback = element_callback
 
@@ -247,7 +247,7 @@ class DataTypeBuilder(_parser.StatementStreamProcessor):
                 "This is to prevent errors if the extent is dependent on the bit length set of the data schema."
             )
 
-    def _on_print_directive(self, line_number: int, value: typing.Optional[_expression.Any]) -> None:
+    def _on_print_directive(self, line_number: int, value: Optional[_expression.Any]) -> None:
         _logger.info(
             "Print directive at %s:%d%s",
             self._definition.file_path,
@@ -256,7 +256,7 @@ class DataTypeBuilder(_parser.StatementStreamProcessor):
         )
         self._print_output_handler(line_number, str(value if value is not None else ""))
 
-    def _on_assert_directive(self, line_number: int, value: typing.Optional[_expression.Any]) -> None:
+    def _on_assert_directive(self, line_number: int, value: Optional[_expression.Any]) -> None:
         if isinstance(value, _expression.Boolean):
             if not value.native_value:
                 raise AssertionCheckFailureError(
@@ -268,7 +268,7 @@ class DataTypeBuilder(_parser.StatementStreamProcessor):
         else:
             raise InvalidDirectiveError("The assertion check expression must yield a boolean, not %s" % value.TYPE_NAME)
 
-    def _on_extent_directive(self, line_number: int, value: typing.Optional[_expression.Any]) -> None:
+    def _on_extent_directive(self, line_number: int, value: Optional[_expression.Any]) -> None:
         if self._structs[-1].serialization_mode is not None:
             raise InvalidDirectiveError(
                 "Misplaced extent directive. The serialization mode is already set to %s"
@@ -284,7 +284,7 @@ class DataTypeBuilder(_parser.StatementStreamProcessor):
         else:
             raise InvalidDirectiveError("The extent directive expects a rational, not %s" % value.TYPE_NAME)
 
-    def _on_sealed_directive(self, _ln: int, value: typing.Optional[_expression.Any]) -> None:
+    def _on_sealed_directive(self, _ln: int, value: Optional[_expression.Any]) -> None:
         if self._structs[-1].serialization_mode is not None:
             raise InvalidDirectiveError(
                 "Misplaced sealing directive. The serialization mode is already set to %s"
@@ -294,7 +294,7 @@ class DataTypeBuilder(_parser.StatementStreamProcessor):
             raise InvalidDirectiveError("The sealed directive does not expect an expression")
         self._structs[-1].set_serialization_mode(_data_schema_builder.SealedSerializationMode())
 
-    def _on_union_directive(self, _ln: int, value: typing.Optional[_expression.Any]) -> None:
+    def _on_union_directive(self, _ln: int, value: Optional[_expression.Any]) -> None:
         if value is not None:
             raise InvalidDirectiveError("The union directive does not expect an expression")
         if self._structs[-1].union:
@@ -303,7 +303,7 @@ class DataTypeBuilder(_parser.StatementStreamProcessor):
             raise InvalidDirectiveError("The union directive must be placed before the first " "attribute definition")
         self._structs[-1].make_union()
 
-    def _on_deprecated_directive(self, _ln: int, value: typing.Optional[_expression.Any]) -> None:
+    def _on_deprecated_directive(self, _ln: int, value: Optional[_expression.Any]) -> None:
         if value is not None:
             raise InvalidDirectiveError("The deprecated directive does not expect an expression")
         if self._is_deprecated:
@@ -322,8 +322,8 @@ class DataTypeBuilder(_parser.StatementStreamProcessor):
         name: str,
         version: _serializable.Version,
         deprecated: bool,
-        fixed_port_id: typing.Optional[int],
-        source_file_path: str,
+        fixed_port_id: Optional[int],
+        source_file_path: Path,
         has_parent_service: bool,
     ) -> _serializable.CompositeType:
         ty = _serializable.UnionType if builder.union else _serializable.StructureType

pydsdl/_dsdl_definition.py
@@ -2,25 +2,27 @@
 # This software is distributed under the terms of the MIT License.
 # Author: Pavel Kirienko <pavel@uavcan.org>
 
+from __future__ import annotations
 import os
 import time
-import typing
+from typing import Iterable, Callable
 import logging
-from . import _error
-from . import _serializable
+from pathlib import Path
+from ._error import FrontendError, InvalidDefinitionError, InternalError
+from ._serializable import CompositeType, Version
 from . import _parser
 
 
 _logger = logging.getLogger(__name__)
 
 
-class FileNameFormatError(_error.InvalidDefinitionError):
+class FileNameFormatError(InvalidDefinitionError):
     """
     Raised when a DSDL definition file is named incorrectly.
     """
 
-    def __init__(self, text: str, path: str):
-        super().__init__(text=text, path=str(path))
+    def __init__(self, text: str, path: Path):
+        super().__init__(text=text, path=Path(path))
 
 
 class DSDLDefinition:
@@ -30,17 +32,17 @@ class DSDLDefinition:
     Upper layers that operate on top of this abstraction do not concern themselves with the file system at all.
     """
 
-    def __init__(self, file_path: str, root_namespace_path: str):
+    def __init__(self, file_path: Path, root_namespace_path: Path):
         # Normalizing the path and reading the definition text
-        self._file_path = os.path.abspath(file_path)
+        self._file_path = Path(file_path).resolve()
         del file_path
-        self._root_namespace_path = os.path.abspath(root_namespace_path)
+        self._root_namespace_path = Path(root_namespace_path).resolve()
         del root_namespace_path
         with open(self._file_path) as f:
             self._text = str(f.read())
 
         # Checking the sanity of the root directory path - can't contain separators
-        if _serializable.CompositeType.NAME_COMPONENT_SEPARATOR in os.path.split(self._root_namespace_path)[-1]:
+        if CompositeType.NAME_COMPONENT_SEPARATOR in os.path.split(self._root_namespace_path)[-1]:
             raise FileNameFormatError("Invalid namespace name", path=self._root_namespace_path)
 
         # Determining the relative path within the root namespace directory
@@ -55,7 +57,7 @@ class DSDLDefinition:
 
         # Parsing the basename, e.g., 434.GetTransportStatistics.0.1.uavcan
         basename_components = basename.split(".")[:-1]
-        str_fixed_port_id = None  # type: typing.Optional[str]
+        str_fixed_port_id: str | None = None
         if len(basename_components) == 4:
             str_fixed_port_id, short_name, str_major_version, str_minor_version = basename_components
         elif len(basename_components) == 3:
@@ -66,10 +68,10 @@ class DSDLDefinition:
         # Parsing the fixed port ID, if specified; None if not
         if str_fixed_port_id is not None:
             try:
-                self._fixed_port_id = int(str_fixed_port_id)  # type: typing.Optional[int]
+                self._fixed_port_id: int | None = int(str_fixed_port_id)
             except ValueError:
                 raise FileNameFormatError(
-                    "Not a valid fixed port-ID: %r. "
+                    "Not a valid fixed port-ID: %s. "
                     "Namespaces are defined as directories; putting the namespace name in the file name will not work. "
                     'For example: "foo/Bar.1.0.uavcan" is OK (where "foo" is a directory); "foo.Bar.1.0.uavcan" is not.'
                     % str_fixed_port_id,
@@ -80,28 +82,26 @@ class DSDLDefinition:
 
         # Parsing the version numbers
         try:
-            self._version = _serializable.Version(major=int(str_major_version), minor=int(str_minor_version))
+            self._version = Version(major=int(str_major_version), minor=int(str_minor_version))
         except ValueError:
             raise FileNameFormatError("Could not parse the version numbers", path=self._file_path) from None
 
         # Finally, constructing the name
         namespace_components = list(relative_directory.strip(os.sep).split(os.sep))
         for nc in namespace_components:
-            if _serializable.CompositeType.NAME_COMPONENT_SEPARATOR in nc:
-                raise FileNameFormatError("Invalid name for namespace component", path=self._file_path)
+            if CompositeType.NAME_COMPONENT_SEPARATOR in nc:
+                raise FileNameFormatError(f"Invalid name for namespace component: {nc!r}", path=self._file_path)
 
-        self._name = _serializable.CompositeType.NAME_COMPONENT_SEPARATOR.join(
-            namespace_components + [str(short_name)]
-        )  # type: str
+        self._name: str = CompositeType.NAME_COMPONENT_SEPARATOR.join(namespace_components + [str(short_name)])
 
-        self._cached_type = None  # type: typing.Optional[_serializable.CompositeType]
+        self._cached_type: CompositeType | None = None
 
     def read(
         self,
-        lookup_definitions: typing.Iterable["DSDLDefinition"],
-        print_output_handler: typing.Callable[[int, str], None],
+        lookup_definitions: Iterable["DSDLDefinition"],
+        print_output_handler: Callable[[int, str], None],
         allow_unregulated_fixed_port_id: bool,
-    ) -> _serializable.CompositeType:
+    ) -> CompositeType:
         """
         Reads the data type definition and returns its high-level data type representation.
         The output is cached; all following invocations will read from the cache.
@@ -153,13 +153,13 @@ class DSDLDefinition:
                 self._cached_type.fixed_port_id,
             )
             return self._cached_type
-        except _error.FrontendError as ex:  # pragma: no cover
+        except FrontendError as ex:  # pragma: no cover
             ex.set_error_location_if_unknown(path=self.file_path)
             raise ex
         except (MemoryError, SystemError):  # pragma: no cover
             raise
         except Exception as ex:  # pragma: no cover
-            raise _error.InternalError(culprit=ex, path=self.file_path)
+            raise InternalError(culprit=ex, path=self.file_path) from ex
 
     @property
     def full_name(self) -> str:
@@ -167,9 +167,9 @@ class DSDLDefinition:
         return self._name
 
     @property
-    def name_components(self) -> typing.List[str]:
+    def name_components(self) -> list[str]:
         """Components of the full name as a list, e.g., ['uavcan', 'node', 'Heartbeat']"""
-        return self._name.split(_serializable.CompositeType.NAME_COMPONENT_SEPARATOR)
+        return self._name.split(CompositeType.NAME_COMPONENT_SEPARATOR)
 
     @property
     def short_name(self) -> str:
@@ -179,7 +179,7 @@ class DSDLDefinition:
     @property
     def full_namespace(self) -> str:
         """The full name without the short name, e.g., uavcan.node for uavcan.node.Heartbeat"""
-        return str(_serializable.CompositeType.NAME_COMPONENT_SEPARATOR.join(self.name_components[:-1]))
+        return str(CompositeType.NAME_COMPONENT_SEPARATOR.join(self.name_components[:-1]))
 
     @property
     def root_namespace(self) -> str:
@@ -192,11 +192,11 @@ class DSDLDefinition:
         return self._text
 
     @property
-    def version(self) -> _serializable.Version:
+    def version(self) -> Version:
         return self._version
 
     @property
-    def fixed_port_id(self) -> typing.Optional[int]:
+    def fixed_port_id(self) -> int | None:
         """Either the fixed port ID as integer, or None if not defined for this type."""
         return self._fixed_port_id
 
@@ -205,11 +205,11 @@ class DSDLDefinition:
         return self.fixed_port_id is not None
 
     @property
-    def file_path(self) -> str:
+    def file_path(self) -> Path:
         return self._file_path
 
     @property
-    def root_namespace_path(self) -> str:
+    def root_namespace_path(self) -> Path:
         return self._root_namespace_path
 
     def __eq__(self, other: object) -> bool:
@@ -222,7 +222,7 @@ class DSDLDefinition:
         return NotImplemented  # pragma: no cover
 
     def __str__(self) -> str:
-        return "DSDLDefinition(full_name=%r, version=%r, fixed_port_id=%r, file_path=%r)" % (
+        return "DSDLDefinition(full_name=%r, version=%r, fixed_port_id=%r, file_path=%s)" % (
             self.full_name,
             self.version,
             self.fixed_port_id,

OpenCyphal/nunavut

commit 2b67f6a8cb730cb4f529dc9296cea9e246cd10bd

Address regression introduced with PyDSDL v1.14 (#248)
* Address regression introduced by https://github.com/UAVCAN/pydsdl/pull/77

* Python 3.6 is no longer supported

* MyPy

* Fix path usage in tests

* Drop Python 3.6

* Fix another test

* Drop Python 3.6 from GitHub Actions, too

* Restore Python 3.6 support

setup.cfg
@@ -36,7 +36,7 @@ package_dir=
     =src
 packages=find:
 install_requires=
-    pydsdl ~= 1.12
+    pydsdl ~= 1.15
     pyyaml
     importlib-resources
 

src/nunavut/__init__.py
@@ -236,8 +236,8 @@ class Namespace(pydsdl.Any):
         return self._full_namespace
 
     @property
-    def source_file_path(self) -> str:
-        return str(self._source_folder)
+    def source_file_path(self) -> pathlib.Path:
+        return self._source_folder
 
     @property
     def data_types(self) -> typing.KeysView[pydsdl.CompositeType]:

src/nunavut/_utilities.py
@@ -15,7 +15,7 @@ A small collection of common utilities.
 import enum
 import logging
 import pathlib
-from typing import Generator
+from typing import Generator, cast
 
 import importlib_resources
 
@@ -104,5 +104,5 @@ def iter_package_resources(pkg_name: str, *suffix_filters: str) -> Generator[pat
 
     """
     for resource in importlib_resources.files(pkg_name).iterdir():
-        if any(suffix == resource.suffix for suffix in suffix_filters):
-            yield resource
+        if any(suffix == resource.suffix for suffix in suffix_filters):  # type: ignore
+            yield cast(pathlib.Path, resource)

src/nunavut/cli/runners.py
@@ -245,11 +245,11 @@ class ArgparseRunner:
         if self._args.generate_support != "only":
             if self.generator.generate_namespace_types:
                 self._stdout_lister(
-                    [x for x, _ in self.root_namespace.get_all_types()], lambda p: str(p.source_file_path)
+                    [x for x, _ in self.root_namespace.get_all_types()], lambda p: p.source_file_path.as_posix()
                 )
             else:
                 self._stdout_lister(
-                    [x for x, _ in self.root_namespace.get_all_datatypes()], lambda p: str(p.source_file_path)
+                    [x for x, _ in self.root_namespace.get_all_datatypes()], lambda p: p.source_file_path.as_posix()
                 )
 
     def _generate(self) -> None:

src/nunavut/lang/c/templates/base.j2
@@ -16,7 +16,7 @@
 // are named with an underscore at the end, like foo_bar_().
 //
 // Generator:     nunavut-{{ nunavut.version }} (serialization was {{ 'not ' * nunavut.support.omit }}enabled)
-// Source file:   {{ T.source_file_path | replace("\\", "\\\\") }}
+// Source file:   {{ T.source_file_path.as_posix() }}
 // Generated at:  {{ now_utc }} UTC
 // Is deprecated: {{ T.deprecated and 'yes' or 'no' }}
 // Fixed port-ID: {{ T.fixed_port_id }}
@@ -54,7 +54,7 @@
 {% endfor %}
 {% for key, value in options.items() -%}
 static_assert( {{ "NUNAVUT_SUPPORT_LANGUAGE_OPTION_{}".format(key) | ln.c.macrofy }} == {{ value | to_static_assertion_value }},
-              "{{ T.source_file_path | replace("\\", "\\\\") }} is trying to use a serialization library that was compiled with "
+              "{{ T.source_file_path.as_posix() }} is trying to use a serialization library that was compiled with "
               "different language options. This is dangerous and therefore not allowed." );
 {% endfor %}
 #ifdef __cplusplus

src/nunavut/lang/cpp/templates/base.j2
@@ -11,7 +11,7 @@
 // build invocation. TODO: add --reproducible option to prevent any volatile metadata from being generated.
 //
 // Generator:     nunavut-{{ nunavut.version }} (serialization was {{ 'not ' * nunavut.support.omit }}enabled)
-// Source file:   {{ T.source_file_path | replace("\\", "\\\\") }}
+// Source file:   {{ T.source_file_path.as_posix() }}
 // Generated at:  {{ now_utc }} UTC
 // Is deprecated: {{ T.deprecated and 'yes' or 'no' }}
 // Fixed port-ID: {{ T.fixed_port_id }}
@@ -73,7 +73,7 @@
 // +-------------------------------------------------------------------------------------------------------------------+
 {% endif -%}
 static_assert( nunavut::support::options::{{ key | id }} == {{ value | ln.c.to_static_assertion_value }},
-              "{{ T.source_file_path | replace("\\", "\\\\") }} "
+              "{{ T.source_file_path.as_posix() }} "
               "is trying to use a serialization library that was compiled with "
               "different language options. This is dangerous and therefore not "
               "allowed." );

src/nunavut/version.py
@@ -7,7 +7,7 @@
 .. autodata:: __version__
 """
 
-__version__ = "1.7.5"  #: The version number used in the release of nunavut to pypi.
+__version__ = "1.7.6"  #: The version number used in the release of nunavut to pypi.
 
 
 __license__ = "MIT"

test/gentest_filters/test_filters.py
@@ -295,14 +295,14 @@ def test_filter_full_reference_name(gen_paths, language_name, stropping, namespa
                                          attributes=[],
                                          deprecated=False,
                                          fixed_port_id=None,
-                                         source_file_path='',
+                                         source_file_path=Path(),
                                          has_parent_service=True)
     service_response_type = StructureType(name='register.getting.tired.of.Python',
                                           version=Version(0, 1),
                                           attributes=[],
                                           deprecated=False,
                                           fixed_port_id=None,
-                                          source_file_path='',
+                                          source_file_path=Path(),
                                           has_parent_service=True)
 
     service_type = ServiceType(service_request_type,

test/gentest_json/templates/StructureType.j2
@@ -7,7 +7,7 @@
       {
         "name": "{{ T.short_name }}",
         "type": "{{ T | typename }}",
-        "source" : "{{ T.source_file_path | replace("\\", "\\\\") }}",
+        "source" : "{{ T.source_file_path.as_posix() }}",
         "fixed_port_id": {{ T.fixed_port_id }},
         "deprecated": {{ T.deprecated | ln.js.to_true_or_false }},
         "version":

test/gentest_namespaces/test_namespaces.py
@@ -93,7 +93,7 @@ def test_empty_namespace(gen_paths):  # type: ignore
     namespace = Namespace('', gen_paths.dsdl_dir, gen_paths.out_dir, LanguageContext(extension='.txt'))
     assert namespace.full_name == ''
     assert namespace.output_folder == gen_paths.out_dir
-    assert namespace.source_file_path == str(gen_paths.dsdl_dir)
+    assert namespace.source_file_path == gen_paths.dsdl_dir
     assert len(namespace.data_types) == 0
     assert (gen_paths.out_dir / Path('_')).with_suffix('.txt') == namespace.find_output_path_for_type(namespace)
     assert namespace == namespace
@@ -110,11 +110,11 @@ def parameterized_test_namespace_(gen_paths, templates_subdir):  # type: ignore
                                   generate_namespace_types=YesNoDefault.NO,
                                   templates_dir=gen_paths.templates_dir / Path(templates_subdir))
     generator.generate_all()
-    assert namespace.source_file_path == root_namespace_path
+    assert namespace.source_file_path == Path(root_namespace_path)
     assert namespace.full_name == 'scotec'
     for nested_namespace in namespace.get_nested_namespaces():
         nested_namespace_path = Path(root_namespace_path) / Path(*nested_namespace.full_name.split('.')[1:])
-        assert nested_namespace.source_file_path == str(nested_namespace_path)
+        assert nested_namespace.source_file_path == nested_namespace_path
 
 
 def test_namespace_any_template(gen_paths):  # type: ignore
@@ -138,7 +138,7 @@ def test_namespace_generation(gen_paths):  # type: ignore
     generator.generate_all()
     for nested_namespace in namespace.get_nested_namespaces():
         nested_namespace_path = Path(root_namespace_path) / Path(*nested_namespace.full_name.split('.')[1:])
-        assert nested_namespace.source_file_path == str(nested_namespace_path)
+        assert nested_namespace.source_file_path == nested_namespace_path
 
     outfile = gen_paths.find_outfile_in_namespace("scotec.mcu", namespace)
 