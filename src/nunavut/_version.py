9f12db40a07db4c55486cd496a7d1dd297a3f280
Pavel Kirienko <pavel.kirienko@gmail.com>
2023-05-21 20:49:37 +0300

    Bump the minor version number

--- src/nunavut/_version.py
+++ src/nunavut/_version.py
@@ -8,7 +8,7 @@
 .. autodata:: __version__
 """
 
-__version__ = "2.1.0"
+__version__ = "2.2.0"
 __license__ = "MIT"
 __author__ = "OpenCyphal"
 __copyright__ = "Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved. Copyright (c) 2023 OpenCyphal."
